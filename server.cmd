976145f9636cb89aa0eeb9a3bd0cc9313341897e
Xevris <rpomicter@smethod.com>
2016-06-21 11:36:12 -0500

    Init

---
+++ server.cmd
@@ -0,0 +1,1 @@
+start "Frost Fables" /d "C:\Users\Richard\Desktop" php -S 0.0.0.0:9000
---
+++ sw.js
@@ -0,0 +1,21 @@
+this.addEventListener('install', e => {
+  caches.open('v1').then( cache => {
+    return cache.addAll([
+      '/ttf-offline/',
+      '/ttf-offline/sw.js',
+      '/ttf-offline/manifest.json',
+      '/ttf-offline/favicon.ico',
+      '/ttf-offline/icon192.png'
+    ]);
+  });
+  console.log('sw installed');
+});
+
+
+this.addEventListener('fetch', e => {
+  if (!e.request.url.includes('192.168.1.8')) {
+    e.respondWith(caches.match(e.request));
+  } else {
+    e.respondWith(fetch(e.request));
+  }
+});
